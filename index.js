8a0e2e342b2b6028f4f4a502eb898c26f8367245
Yetunde Alese <yaalese1@gmail.com>
2022-11-03 22:31:44 -0400

    commit tree 8bb4ab30944eb48c426c71ebc9bf48fb85132e33

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+ 
+function writeCards(names, event){
+  let cardMessages = []
+  for (let i = 0; i < names.length; i++) {
+   cardMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+     
+   }
+ 
+   return cardMessages;
+}
+
+
+function countDown(n){
+while (n >= 0)
+ console.log(n--)
+}
+
+countDown(5)
